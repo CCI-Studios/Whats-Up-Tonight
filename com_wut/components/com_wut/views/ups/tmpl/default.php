576aa027e51b07437e1052a309b16293e6d1d525
Jonathan Bennett <jbennett@ccistudios.com>
2012-06-28 14:58:39 -0400

    Fixer modules dir

--- com_wut/components/com_wut/views/ups/tmpl/default.php
+++ com_wut/components/com_wut/views/ups/tmpl/default.php
@@ -1,25 +1,29 @@
+<style src="media://com_wut/css/com_wut_site.css" />
 
-<div class="row">
+<div class="row com_wut-ups">
 <? foreach($ups as $up): ?>
-	<div class="module span4 grid">
-		<div><div><div>
-			<div style="height: <?= $height ?>" class="custom"><div>
-				<h3><?= $up->title ?></h3>
-				
-				<div class="logo"><img src="/images/logos/logo_scroggies.png" width="127" height="98" alt="logo scroggies"></div>
-
-				<!-- <div class="title"><h4>Eats/Drinks</h4></div> -->
+	<div class="module span4 grid"><div><div>
+		<div style="height: <?= $height ?>" class="custom">
+			<h3><?= $up->title ?></h3>
+			
+			<div class="description1">
+				<div class="logo">
+					<img src="/images/logos/logo_scroggies.png" width="127" height="98" alt="logo scroggies">
+				</div>
 
 				<div class="description"><?= $up->intro ?></div>
+			</div>
 
-				<div><button>
-					Details
-					<img src="/images/arrowDown.png" width="10" height="8" alt="arrowDown">
-				</button></div>
+			<div class="description2">
+				adsfasdffsda
+			</div>
 
-			</div></div>
-		</div></div></div>
-	</div>
+			<div><button>
+				Details
+				<img src="/images/arrowDown.png" width="10" height="8" alt="arrowDown">
+			</button></div>
+		</div>
+	</div></div></div>
 <? endforeach; ?>
 </div>
 
---
+++ com_wut/modules/mod_wutdates/tmpl/default.php
@@ -0,0 +1,53 @@
+
+<div class="mod_wutdates">
+	<div class="prev"><div>
+		<a href="#"></a>
+	</div></div>
+
+	<div class="item first"><a href="#">
+		<div class="day">Monday</div>
+		<div class="date">12</div>
+		<div class="ups">16 UPs</div>
+	</a></div>
+
+	<div class="item"><a href="#">
+		<div class="day">Tuesday</div>
+		<div class="date">13</div>
+		<div class="ups">12 UPs</div>
+	</a></div>
+
+	<div class="item"><a href="#">
+		<div class="day">Wednesday</div>
+		<div class="date">14</div>
+		<div class="ups">12 UPs</div>
+	</a></div>
+
+	<div class="item featured"><a href="#">
+		<div class="day">Thursday</div>
+		<div class="date">15</div>
+		<div class="ups">14 UPs</div>
+		<div class="logo-featured"></div>
+	</a></div>
+
+	<div class="item"><a href="#">
+		<div class="day">Friday</div>
+		<div class="date">16</div>
+		<div class="ups">17 UPs</div>
+	</a></div>
+
+	<div class="item"><a href="#">
+		<div class="day">Saturday</div>
+		<div class="date">17</div>
+		<div class="ups">18 UPs</div>
+	</a></div>
+
+	<div class="item last"><a href="#">
+		<div class="day">Sunday</div>
+		<div class="date">18</div>
+		<div class="ups">9 UPs</div>
+	</a></div>
+
+	<div class="next"><div>
+		<a href="#"></a>
+	</div></div></div>
+</div>
