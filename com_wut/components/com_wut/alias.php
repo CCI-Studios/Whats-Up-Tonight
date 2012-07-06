d83a53e5e39fa936e06d8289d0f0d0107636b852
Jonathan Bennett <jbennett@ccistudios.com>
2012-07-06 14:26:51 -0400

    Updated aliases

--- com_wut/components/com_wut/alias.php
+++ com_wut/components/com_wut/alias.php
@@ -3,3 +3,6 @@ defined('KOOWA') or die;
 
 KService::setAlias('com://site/wut.model.locations',	'com://admin/wut.model.locations');
 KService::setAlias('com://site/wut.model.ups',			'com://admin/wut.model.ups');
+
+KService::setAlias('com://site/wut.database.row.location',	'com://admin/wut.database.row.location');
+KService::setAlias('com://site/wut.database.row.up',		'com://admin/wut.database.row.up');
