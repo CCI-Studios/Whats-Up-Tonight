529eb974b376e8584bb9075a9eadd06f00651d23
Jonathan Bennett <jbennett@ccistudios.com>
2012-07-09 12:05:08 -0400

    Updated locations layout

--- com_wut/components/com_wut/views/location/tmpl/simple.php
+++ com_wut/components/com_wut/views/location/tmpl/simple.php
@@ -11,19 +11,16 @@
 	<div class="content">
 		<a href="<?= $location->map ?>" target="_blank"><?= $location->fullAddress() ?></a> 
 		<a href="<?= $location->map ?>" target="_blank">Map</a><br/>
-		<?= ($location->postal)? $location->postal.'<br>' : '' ?>
 		<?= ($location->phone)? $location->phone.'<br>' : '' ?>
 		
 		<? if ($location->website): ?>
 			<a href="<?= $location->website ?>" target="_blank">
-				<?= $location->website ?>
-			</a>
+				<?= $location->website ?></a><br/>
 		<? endif; ?>
 
 		<? if ($location->reviews): ?>
 			<a href="<?= $location->reviews ?>" target="_blank">
-				<?= $location->reviews ?>
-			</a>
+				<?= $location->reviews ?></a><br/>
 		<? endif; ?>
 	</div>
 </div>
--- com_wut/components/com_wut/views/locations/tmpl/default.php
+++ com_wut/components/com_wut/views/locations/tmpl/default.php
@@ -2,17 +2,17 @@
 <script src="media://lib_koowa/js/koowa.js" />
 
 <form action="<?= @route() ?>" method="get" class="-koowa-grid">
-	<ul>
-		<? foreach ($locations as $location): ?>
-			<li><a href="<?= @route("view=location&id={$location->id}")?>">
-				<?= $location->title ?>
-			</a></li>
-		<? endforeach; ?>
 
-		<? if (!count($locations)) :?>
-			<li>No Locations</li>
-		<? endif; ?>
-	</ul>
+	<? if (count($locations)): ?>
+		<? foreach($locations as $location): ?>
+			<h2><?= $location->title ?></h2>
+			<?= @template('com://site/wut.view.location.simple', array('location'=>$location)) ?>
+			<div class="right"><a href="<?= @route("view=location&id={$location->id}") ?>">More Details</a></div>
+		<? endforeach; ?>
+	<? else: ?>
+		<p>No locations</p>
+	<? endif; ?>
+	<div class="clear" style="margin: 1em 0 0;"></div>
 
 	<div class="pagination">
 		<?= @helper('paginator.pagination', array(
