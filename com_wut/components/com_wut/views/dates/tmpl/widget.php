5effb79165ec5930d247980aa6b47d34dc2a8518
Jonathan Bennett <jbennett@ccistudios.com>
2012-07-09 02:37:23 -0400

    Added space between count and label
    Fixes #7

--- com_wut/components/com_wut/views/dates/tmpl/widget.php
+++ com_wut/components/com_wut/views/dates/tmpl/widget.php
@@ -12,7 +12,7 @@
 				<a href="<?= @route("view=ups&{$itemid}&date=". date('Y-m-d', $day['date'])) ?>">
 					<div class="day"><?= date('l', $day['date']) ?></div>
 					<div class="date"><?= date('d', $day['date']) ?></div>
-					<div class="ups"><?= $day['count'] ?><?= ($day['count'] > 1)? 'UPs':'UP' ?></div>
+					<div class="ups"><?= $day['count'] ?><?= ($day['count'] > 1)? ' UPs':' UP' ?></div>
 				</a>
 			</div>
 		<? endforeach; ?>
