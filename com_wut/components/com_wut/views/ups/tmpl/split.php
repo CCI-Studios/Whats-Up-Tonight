abcd3734b8741c13398c3f65448491f324f4c129
Jonathan Bennett <jbennett@ccistudios.com>
2012-07-09 08:59:43 -0400

    Split out Ups view for module reuse

--- com_wut/components/com_wut/views/ups/tmpl/split.php
+++ com_wut/components/com_wut/views/ups/tmpl/split.php
@@ -2,32 +2,7 @@
 	<? if (count($ups)): ?>
 		<div class="row">
 		<? foreach($ups as $up): ?>
-			<div class="span4"><div>
-				<div style="height: <?= $height ?>">
-					<h3><?= $up->title ?></h3>
-					
-					<div class="description1">
-						<div class="logo">
-							<?= $up->location->image(); ?>
-						</div>
-
-						<div class="description"><?= $up->intro ?></div>
-					</div>
-
-					<div class="description2">
-						<h4><?= $up->subtitle ?></h4>
-						<p><?= $up->short ?></p>
-						<p><a href="<?= @route("view=up&{$upid}&id={$up->id}") ?>">
-							<?= @text('Read more') ?>
-						</a></p>
-					</div>
-
-					<div class="details"><div class="button">
-						Details
-						<img src="/images/arrowDown.png" width="10" height="8" alt="arrowDown">
-					</div></div>
-				</div>
-			</div></div>
+			<?= @template('widget', array('up'=>$up)); ?>
 		<? endforeach; ?>
 		</div>
 	<? else: ?>
