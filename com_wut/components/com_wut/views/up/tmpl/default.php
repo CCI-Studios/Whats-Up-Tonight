9dad9f62887c63f1bd453b7071db468a5897564a
Chris Morris <cmorris@ccistudios.com>
2013-12-09 12:46:57 -0500

    Fix date on UP details page for repeating events

--- com_wut/components/com_wut/views/up/tmpl/default.php
+++ com_wut/components/com_wut/views/up/tmpl/default.php
@@ -1,7 +1,13 @@
+<?php
+$date = '';
+if ($up->date != '0000-00-00')
+  $date = ' - ' . date('l F j, Y', strtotime($up->date));
+?>
+
 <style src="media://com_wut/css/com_wut_site.css" />
 
 <div class="item-page">
-	<h1><?= "{$up->title} - ". date('l F j, Y', strtotime($up->date)) ?></h1>
+	<h1><?= "{$up->title}". $date ?></h1>
 
 	<?= @template('com://site/wut.view.location.simple', array('location'=>$up->location)) ?>
 
